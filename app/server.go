e333917787e411cbec5578960a57c97469c86ae3
KhushPatibandha <khush.patibandha@gmail.com>
2024-06-09 16:17:53 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
@@ -19,5 +21,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"));
+	handleConn(connection);
+}
+
+func handleConn(connection net.Conn) {
+
+	defer connection.Close();
+	req, err := http.ReadRequest(bufio.NewReader(connection));
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		return
+	}
+
+	fmt.Println("Request method: ", req.Method);
+	fmt.Println("Request url: ", req.URL.Path);
+
+	if req.URL.Path == "/" {
+		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"));
+	}
+	
+	connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"));
 }
